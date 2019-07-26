bf5935cf074e02bfb570a5194237369cdb3a2199
katherinestout <kjs14@hood.edu>
2019-07-26 14:26:58 -0400

    authenticate user

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+
+//exporting middleware function that has req res object
+//get token from header req.header
+//if no token at all, send 401 error
+//if is token not valid just run catch
+//if valid it will decode it
+//set decoded to req.user
+
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
