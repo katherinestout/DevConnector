06104fbbf8ad30aa5fd24376d916675af4f261be
katherinestout <kjs14@hood.edu>
2019-07-26 13:23:42 -0400

    implementing jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {check, validationResult} = require('express-validator');
 
 const User = require('../../models/User');
@@ -9,6 +11,15 @@ const User = require('../../models/User');
 //@route POST api/users
 //@desc  Register user
 //@acess Public
+
+
+//create user
+//hash password
+//save user
+//get payload
+//sign token pass in payload and secret
+//inside callback get err or token
+//if token, get back token
 
 router.post('/',[
     check('name', 'Name is required').not().isEmpty(),
@@ -19,39 +30,48 @@ router.post('/',[
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
-
     const {name, email, password} = req.body;
-
     try{
         let user = await User.findOne({ email });
 
         if(user){
             res.status(400).json({errors: [{msg: "user already exists"}]});
         }
-
         const avatar = gravatar.url(email, {
             s: '200',
             r: 'pg',
             d: 'mm'
         })
-
         user = new User({
             name,
             email,
             avatar,
             password
         });
-
         const salt = await bcrypt.genSalt(10);
 
         //hash password
         user.password = await bcrypt.hash(password, salt);
-
         await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            {expiresIn: 36000},
+            (err, token) => {
+                if(err) throw err;
+                res.json({token});
+
+            });
 
         //return jsonwebtoken
 
-        res.send('User Registered!')
+       // res.send('User Registered!')
     } catch(err){
         console.error(err.message);
         res.status(500).send('Server Error');
