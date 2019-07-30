034cf093b415e8151d12d90cb8ada54c7ba75977
katherinestout <kjs14@hood.edu>
2019-07-30 15:17:51 -0400

    profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-//@route GET api/profile
-//@desc  Test route
-//@acess Public
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => res.send('profile route'));
+//@route GET api/profile/me
+//@desc  Get current users profile
+//@acess Private
+
+//add auth for protected routes
+
+router.get('/me', auth, async (req, res) => 
+{
+    try {
+        const profile = await Profile.findOne({user: req.user.id}).populate('user',
+        ['name', 'avatar']);
+        if(!profile){
+            return res.status(400).json({msg: 'There is no profile for this user'});
+        }
+        res.json(profile);
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
